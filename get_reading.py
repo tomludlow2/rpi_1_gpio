2b551b2b9aa7f9380559be47a802a825f440bd12
Tom Ludlow <tom.m.lud@gmail.com>
2021-09-16 16:51:36 +0100

    Created a modular structure

---
+++ get_reading.py
@@ -0,0 +1,158 @@
+import RPi.GPIO as GPIO
+import time
+
+class rpi_gpio_class:
+    #DHT11 connect to BCM_GPIO14 (Pin 8 on RPi 1)
+    DHTPIN = 14
+
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setwarnings(False)
+
+    MAX_UNCHANGE_COUNT = 100
+
+    STATE_INIT_PULL_DOWN = 1
+    STATE_INIT_PULL_UP = 2
+    STATE_DATA_FIRST_PULL_DOWN = 3
+    STATE_DATA_PULL_UP = 4
+    STATE_DATA_PULL_DOWN = 5
+
+    def read_dht11_dat(self):
+        GPIO.setup(self.DHTPIN, GPIO.OUT)
+        GPIO.output(self.DHTPIN, GPIO.HIGH)
+        time.sleep(0.05)
+        GPIO.output(self.DHTPIN, GPIO.LOW)
+        time.sleep(0.02)
+        GPIO.setup(self.DHTPIN, GPIO.IN, GPIO.PUD_UP)
+
+        unchanged_count = 0
+        last = -1
+        data = []
+        while True:
+            current = GPIO.input(self.DHTPIN)
+            data.append(current)
+            if last != current:
+                unchanged_count = 0
+                last = current
+            else:
+                unchanged_count += 1
+                if unchanged_count > self.MAX_UNCHANGE_COUNT:
+                    break
+
+        state = self.STATE_INIT_PULL_DOWN
+
+        lengths = []
+        current_length = 0
+
+        for current in data:
+            current_length += 1
+
+            if state == self.STATE_INIT_PULL_DOWN:
+                if current == GPIO.LOW:
+                    state = self.STATE_INIT_PULL_UP
+                else:
+                    continue
+            if state == self.STATE_INIT_PULL_UP:
+                if current == GPIO.HIGH:
+                    state = self.STATE_DATA_FIRST_PULL_DOWN
+                else:
+                    continue
+            if state == self.STATE_DATA_FIRST_PULL_DOWN:
+                if current == GPIO.LOW:
+                    state = self.STATE_DATA_PULL_UP
+                else:
+                    continue
+            if state == self.STATE_DATA_PULL_UP:
+                if current == GPIO.HIGH:
+                    current_length = 0
+                    state = self.STATE_DATA_PULL_DOWN
+                else:
+                    continue
+            if state == self.STATE_DATA_PULL_DOWN:
+                if current == GPIO.LOW:
+                    lengths.append(current_length)
+                    state = self.STATE_DATA_PULL_UP
+                else:
+                    continue
+        if len(lengths) != 40:
+            return False
+
+        shortest_pull_up = min(lengths)
+        longest_pull_up = max(lengths)
+        halfway = (longest_pull_up + shortest_pull_up) / 2
+        bits = []
+        the_bytes = []
+        byte = 0
+
+        for length in lengths:
+            bit = 0
+            if length > halfway:
+                bit = 1
+            bits.append(bit)
+        for i in range(0, len(bits)):
+            byte = byte << 1
+            if (bits[i]):
+                byte = byte | 1
+            else:
+                byte = byte | 0
+            if ((i + 1) % 8 == 0):
+                the_bytes.append(byte)
+                byte = 0
+        checksum = (the_bytes[0] + the_bytes[1] + the_bytes[2] + the_bytes[3]) & 0xFF
+        if the_bytes[4] != checksum:
+            return False
+
+        return the_bytes[0], the_bytes[2]
+
+
+    def __init__(self):
+        print ("Class initiated")
+
+    def get_reading(self):
+        print "Raspberry Pi Temperature and Humidity Program\n"
+        i = 0
+        temp = {}
+        humid = {}
+        while i < 1:
+            result = self.read_dht11_dat()
+            if result:
+                humidity, temperature = result
+                temp['temperature'] = temperature
+                humid['humidity'] = humidity
+                print "Reading: Humidity: %s %%,  Temperature: %s C" % (humidity, temperature)
+                i =  i+1        
+        return [temp,humid]
+
+    def destroy():
+        GPIO.clean()
+
+    def get_current_readings(self):
+        print "Getting current readings"
+
+
+
+
+def main():
+    print "Raspberry Pi Temperature and Humidity Program\n"
+    i = 0
+    num = int(input("How many readings do you want to take? "))
+    temp_reads = []
+    humidity_reads = []
+    while i < num:
+        result = read_dht11_dat()
+        if result:
+            humidity, temperature = result
+            temp_reads.append(temperature)
+            humidity_reads.append(humidity)
+            print "Reading: Humidity: %s %%,  Temperature: %s C" % (humidity, temperature)
+            i =  i+1
+    
+    temp_average = round(sum(temp_reads) / len(temp_reads), 1)
+    humidity_average = round(sum(humidity_reads) / len(humidity_reads) ,1)
+    print "Success: Completed Data Acquisition"
+    print "Temperature Readings: "
+    print temp_reads
+    print( "Average Temp: ", temp_average, "deg C")
+
+    print "Humuditity Readings: "
+    print humidity_reads
+    print( "Average Humidity: ", humidity_average, "%");
